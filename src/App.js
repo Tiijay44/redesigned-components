cb2aa45e1f20da1f05b59e73b34d74cdbc02b641
EseneAnthony <eseneanthony@gmail.com>
2024-02-17 12:37:02 +0100

    initial buttons designs

--- src/App.js
+++ src/App.js
@@ -1,33 +1,56 @@
 import Button from "./Button";
+import { GiClick } from "react-icons/gi";
+import { GiBuyCard } from "react-icons/gi";
+import { RiDeleteBin6Line } from "react-icons/ri";
+import { FaSave } from "react-icons/fa";
+import { CgDanger } from "react-icons/cg";
+import { IoIosCloudDone } from "react-icons/io";
 
 function App() {
+  const handleClick = () => console.log("Click");
   return (
     <div>
       <div>
-        <Button primary secondary rounded outline>
+        <Button
+          primary
+          rounded
+          outline
+          className="mb-5 mt-5"
+          onClick={handleClick}
+        >
+          <GiClick />
           Click here!!
         </Button>
       </div>
       <div>
-        <Button secondary rounded>
+        <Button secondary outline rounded>
+          <GiBuyCard />
           Buy me!
         </Button>
       </div>
       <div>
         <Button success outline>
+          <RiDeleteBin6Line />
           Delete
         </Button>
       </div>
       <div>
-        <Button warning>Save</Button>
+        <Button warning>
+          <FaSave />
+          Save
+        </Button>
       </div>
       <div>
         <Button danger outline rounded>
+          <CgDanger />
           Hide Ads!
         </Button>
       </div>
       <div>
-        <Button>Submit!</Button>
+        <Button rounded>
+          <IoIosCloudDone />
+          Submit!
+        </Button>
       </div>
     </div>
   );
--- src/Button.js
+++ src/Button.js
@@ -1,4 +1,7 @@
-import PropTypes from "prop-types";
+// import PropTypes from "prop-types";
+import className from "classnames";
+import { twMerge } from "tailwind-merge";
+
 function Button({
   primary,
   secondary,
@@ -8,30 +11,52 @@ function Button({
   rounded,
   outline,
   children,
+  ...rest
 }) {
-  return <button>{children}</button>;
+  const classes = twMerge(
+    className(rest.className, "flex items-center border px-3 py-1.5", {
+      "bg-blue-500 border-blue-500 text-white": primary,
+      "bg-gray-900 border-gray-900 text-white": secondary,
+      "bg-green-500 border-green-500 text-white": success,
+      "bg-yellow-500 border-yellow-500 text-white": warning,
+      "bg-red-500 border-red-500 text-white": danger,
+      "rounded-full": rounded,
+      "bg-white": outline,
+      "text-blue-500": primary && outline,
+      "text-gray-900": secondary && outline,
+      "text-green-500": success && outline,
+      "text-yellow-500": warning && outline,
+      "text-red-500": danger && outline,
+    })
+  );
+
+  return (
+    <button {...rest} className={classes}>
+      {children}
+    </button>
+  );
 }
 
-Button.propTypes = {
-  variationChecker: function ({
-    primary,
-    secondary,
-    success,
-    warning,
-    danger,
-  }) {
-    const count =
-      Number(!!primary) +
-      Number(!!secondary) +
-      Number(!!success) +
-      Number(!!warning) +
-      Number(!!danger);
-    if (count > 1) {
-      throw new Error(
-        "Only one of primary, secondary, success, warning or danger should be true!"
-      );
-    }
-  },
-};
+// Button.propTypes = {
+//   variationChecker: function ({
+//     primary,
+//     secondary,
+//     success,
+//     warning,
+//     danger,
+//   }) {
+//     const count =
+//       Number(!!primary) +
+//       Number(!!secondary) +
+//       Number(!!success) +
+//       Number(!!warning) +
+//       Number(!!danger);
+//     if (count > 1) {
+//       throw new Error(
+//         "Only one of primary, secondary, success, warning or danger should be true!"
+//       );
+//     }
+//   },
+// };
 
 export default Button;
